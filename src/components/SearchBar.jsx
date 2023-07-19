d2c24abd7905b222dba57d527e07fcfb489fbe60
Khash <khashayar.m.003@gmail.com>
2023-07-19 07:27:32 +0430

    logo added , fixin navbar and searchbar

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import {Paper , IconButton} from '@mui/material';
+import { SearcIcon } from '@mui/icons-material';
+
+const SearchBar = () => {
+  return (
+    <Paper>SearchBar</Paper>
+  )
+}
+
+export default SearchBar
