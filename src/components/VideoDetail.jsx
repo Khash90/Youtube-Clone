20952e7aa15efe8d7aeec2e6927995f1fd0cd511
Khash <khashayar.m.003@gmail.com>
2023-07-25 01:01:04 +0430

    done

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -33,7 +33,7 @@ const VideoDetail = () => {
         <Box sx={{ width:'100%', position: 'sticky', top: '86px' }}>
           <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`} 
             className='react-player' controls />
-          <Typography color='#fff' variant='h5' fontWeight='bold' p={2}>
+          <Typography color='#fff' variant="h5" fontWeight='bold' p={2}>
              {title}
           </Typography>  
           <Stack direction='row' justifyContent='space-between' sx={{
