fa630e004cac7867f6f83bed079b657dda3cdb14
Khash <khashayar.m.003@gmail.com>
2023-07-24 20:46:33 +0430

    search function is done and working

--- src/App.js
+++ src/App.js
@@ -1,33 +1,28 @@
-
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import {Box} from '@mui/material';
-import { Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed } from './components'
-
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
+import {
+  Navbar,
+  Feed,
+  VideoDetail,
+  ChannelDetail,
+  SearchFeed,
+} from "./components";
 
 const App = () => (
-  
-    <BrowserRouter>
-        <Box sx={{ backgroundColor: '#000' }}>
+  <BrowserRouter>
+    <Box sx={{ backgroundColor: "#000" }}>
+      <Navbar />
+      <Routes>
+        <Route path="/" exact element={<Feed />} />
 
-            <Navbar />
-            <Routes>
+        <Route path="/video/:id" element={<VideoDetail />} />
 
-                <Route path='/' exact element={<Feed />} />
+        <Route path="/channel/:id" element={<ChannelDetail />} />
 
-                <Route path='/video/:id' element={<VideoDetail />} />
+        <Route path="/search/:searchTerm" element={<SearchFeed />} />
+      </Routes>
+    </Box>
+  </BrowserRouter>
+);
 
-                <Route path='/channel/:id' element={<ChannelDetail />} />
-
-                <Route path='/search/:searchTerm' element={<SearchFeed />} />
-
-
-            </Routes>
-
-
-        </Box>
-    </BrowserRouter>
-)
-
-
-export default App
+export default App;
